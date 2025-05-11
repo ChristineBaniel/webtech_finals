f4a78566f6eeb3837cdfe75d97c8afca42b3a58a
ChristineBaniel <christinebaniel0909@gmail.com>
2025-05-11 14:53:28 +0800

    m

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ module.exports = {
   output: 'standalone',
   // Remove these after fixing errors:
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  // Add if using ISR:
-  experimental: { isrMemoryCacheSize: 50 }
+  typescript: { ignoreBuildErrors: true }
 }
